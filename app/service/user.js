ae9a573faaac39e5b74a87c8e00bff3305b1620f
yuzhen.peng <yuzhen.peng.dev@gmail.com>
2019-04-23 03:01:52 +0800

    ...

--- app/service/user.js
+++ app/service/user.js
@@ -5,32 +5,6 @@ var myDate = new Date()
 myDate.setFullYear(2019, 3, 22)
 
 class UserService extends Service {
-    // async getAll(param) {
-    //     if (param.region == 'NO') {
-    //         const results = await this.app.mysql.select('total', {
-    //             orders: ['report_id'],
-    //             limit: 10,
-    //             offset: 10 * (param.pageNum || 1 - 1),
-    //         });
-    //         const totalNum = await this.app.mysql.select('total', {
-    //             orders: ['report_id'],
-    //         });
-    //         console.log(totalNum);
-    //         return { results, totalNum };
-    //     } else {
-    //         const results = await this.app.mysql.select('total', {
-    //             where: { report_region: param.region },
-    //             orders: ['report_id'],
-    //             limit: 10,
-    //             offset: 10 * (param.pageNum || 1 - 1),
-    //         });
-    //         const totalNum = await this.app.mysql.select('total', {
-    //             where: { report_region: param.region },
-    //             orders: ['report_id'],
-    //         });
-    //         return { results, totalNum };
-    //     }
-    // }
     async getAll(param) {
         var res, results, totalNum, sort, differ;
         sort = param.sort == 'down' ? 'date desc' : param.sort == 'up' ? 'date' : 'date desc';
@@ -41,7 +15,7 @@ class UserService extends Service {
                 `select id, region, date, url, title, abstract from zimaoqu where TO_DAYS(NOW()) - TO_DAYS(date) <= ${differ} and abstract like '%${param.keywords}%' order by ${sort}`,
             );
             totalNum = res.length;
-            results = res.slice(10 * (param.pageNum), 10 + 10 * (param.pageNum));
+            results = res.slice(6 * (param.pageNum), 6 + 6 * (param.pageNum));
             return { results, totalNum };
         } else {
             res = await this.app.mysql.query(
@@ -49,7 +23,7 @@ class UserService extends Service {
                 `select id, region, date, url, title, abstract from zimaoqu where region = '${param.region}' and TO_DAYS(NOW()) - TO_DAYS(date) <= ${differ} and abstract like '%${param.keywords}%' order by ${sort}`,
             );
             totalNum = res.length;
-            results = res.slice(10 * (param.pageNum), 10 + 10 * (param.pageNum));
+            results = res.slice(6 * (param.pageNum), 6 + 6 * (param.pageNum));
             return { results, totalNum };
         }
     }
