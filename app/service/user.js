3f8434a4aa1691f7a36deed6b9ed64e3f982f101
yuzhen.peng <yuzhen.peng.dev@gmail.com>
2019-04-23 08:17:04 +0800

    ...

--- app/service/user.js
+++ app/service/user.js
@@ -11,7 +11,6 @@ class UserService extends Service {
         differ = param.differ == '1' ? 30 : param.differ == '2' ? 90 : param.differ == '3' ? 365 : 73000;
         if (param.region == 'all') {
             res = await this.app.mysql.query(
-                // `select id, region, date, url, title, abstract from zimaoqu where region = '${param.region}' and abstract like '%${param.keywords}%'  and date < ${newDate}  and id limit ` + ((param.pageNum || 1 - 1) * 10) + "," + 10,
                 `select id, region, date, url, title, abstract from zimaoqu where TO_DAYS(NOW()) - TO_DAYS(date) <= ${differ} and abstract like '%${param.keywords}%' order by ${sort}`,
             );
             totalNum = res.length;
@@ -19,7 +18,6 @@ class UserService extends Service {
             return { results, totalNum };
         } else {
             res = await this.app.mysql.query(
-                // `select id, region, date, url, title, abstract from zimaoqu where region = '${param.region}' and abstract like '%${param.keywords}%'  and date < ${newDate}  and id limit ` + ((param.pageNum || 1 - 1) * 10) + "," + 10,
                 `select id, region, date, url, title, abstract from zimaoqu where region = '${param.region}' and TO_DAYS(NOW()) - TO_DAYS(date) <= ${differ} and abstract like '%${param.keywords}%' order by ${sort}`,
             );
             totalNum = res.length;
